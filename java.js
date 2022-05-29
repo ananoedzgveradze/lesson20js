34d9a1679d0266464be94b5e310b3ce698d6758b
ananoedzgveradze <102252416+ananoedzgveradze@users.noreply.github.com>
2022-05-29 18:38:24 +0400

    add java

--- java.js
+++ java.js
@@ -1,17 +1,22 @@
-// let accordion1 = document.getElementsByClassName('container');
-
-// for (let i = 0; i < accordion1.length; i++) {
-//    accordion1[i].addEventListener('click', function () {
-//        this.classList.add('active');
-//    })
-// }
-
-
 let accordion = document.getElementsByClassName('container');
 
 for (let i = 0; i < accordion.length; i++) {
-    accordion[i].addEventListener('click', function () {
-        this.classList.add('active');
-    });
-    
+    accordion[i].addEventListener('click', function() {
+        this.classList.toggle('active');
+    })
 }
+
+//burger icon
+
+let navigation = document.getElementById ('navbarlinks');
+let toggleButton = document.getElementById ('toggleBurger');
+let upLine = document.getElementById('burger1');
+let middleLine = document.getElementById('burger2');
+let downLine = document.getElementById('burger3');
+
+toggleButton.addEventListener('click', function () {
+    navigation.classList.toggle('activeNavigation');
+    upLine.classList.toggle('up');
+    middleLine.classList.toggle('middle');
+    downLine.classList.toggle('down');
+});
